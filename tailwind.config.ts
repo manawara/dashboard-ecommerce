c3c1432824fc0d1070818e9045bf2a8f00d1858f
mnawara <marcinnawara@int.pl>
2024-10-19 23:41:55 +0200

    Configure global color for tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,19 +9,57 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        primary: "#ff6c2f",
-        secondary: "#5d7186",
-        green: "#22c55e",
-        blue: "#4ecac2",
-        red: "#ef5f5f",
-        yellowLight: "rgba(255, 108, 47, 0.1)",
-        yellow: "#f9b931",
-        grayLightest: "#F8F7F7",
-        grayLight: "#d8dfe7",
-        gray: "#b0b0bb",
-        grayDark: "#323a46",
-        purple: "#7f56da",
-        pink: "#ff86c8",
+        primary: {
+          DEFAULT: "#ff6c2f",
+          hover: "#e6612a",
+          soft: "rgba(255, 108, 47, 0.1)",
+        },
+        secondary: {
+          DEFAULT: "#5d7186",
+          hover: "#546679",
+          soft: "rgba(93, 113, 134, 0.1)",
+        },
+        green: {
+          DEFAULT: "#22c55e",
+          hover: "#1fb155",
+          soft: "rgba(34, 197, 94, 0.1)",
+        },
+        blue: {
+          DEFAULT: "#4ecac2",
+          hover: "#46b6af",
+          soft: "rgba(78, 202, 194,0.1)",
+          dark: {
+            DEFAULT: "#313b5e",
+          },
+        },
+        red: {
+          DEFAULT: "#ef5f5f",
+          hover: "#d75656",
+        },
+        yellow: {
+          DEFAULT: "#f9b931",
+          hover: "#e0a72c",
+          soft: "rgba(249, 185, 49, 0.1)",
+        },
+
+        gray: {
+          grayLight: {
+            DEFAULT: "#d8dfe7",
+            300: "#F8F7F7",
+          },
+          DEFAULT: "#b0b0bb",
+          dark: {
+            DEFAULT: "#323a46",
+          },
+        },
+        purple: {
+          DEFAULT: "#7f56da",
+          hover: "#724dc4",
+        },
+        pink: {
+          DEFAULT: "#ff86c8",
+          hover: "#e679b4",
+        },
       },
       fontFamily: {
         play: ["var(--font-play)"],
