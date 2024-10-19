78bac80e22e1dc49c533fe67f736b0eeb4606ad4
mnawara <marcinnawara@int.pl>
2024-10-19 23:45:06 +0200

    Add new component FormLogin

---
+++ app/_components/Auth/FormLogin.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Input from "../Form/Input/Input";
+import { Mail, KeyRound } from "lucide-react";
+import Button from "../Button/Button";
+import Checkbox from "../Form/Checkbox/Checkbox";
+import facebookIcon from "@/public/facebook.svg";
+const FormLogin = () => {
+  return (
+    <section className="mt-4">
+      <h1 className="font-semibold text-2xl text-blueDark">Sign In</h1>
+      <p className="text-sm font-medium mt-2 mb-5">
+        Enter your email address and password to access admin panel.
+      </p>
+      <form className=" flex flex-col gap-6">
+        <Input
+          type="email"
+          name="email"
+          placeholder="Enter your email"
+          label="Email"
+          icon={<Mail className="text-inherit" strokeWidth={1} size={20} />}
+        />
+
+        <Input
+          type="password"
+          name="password"
+          placeholder="Enter your password"
+          label="Password"
+          icon={<KeyRound className="text-inherit" strokeWidth={1} size={20} />}
+        />
+
+        <Checkbox label="Remember me" name="rememberMe" />
+
+        <Button rounded="xl" size="full" soft="orange">
+          Sign In
+        </Button>
+      </form>
+    </section>
+  );
+};
+
+export default FormLogin;
