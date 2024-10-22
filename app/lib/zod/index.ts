fcb2a6887084fd5f9d93a7e89edf89c29da468d2
mnawara <marcinnawara@int.pl>
2024-10-22 23:03:34 +0200

    Create login and register schema

---
+++ app/lib/zod/index.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+export const loginSchema = z.object({
+  email: z.string().min(1, "Email is required!"),
+  password: z.string().min(1, "Password is required!"),
+});
+
+export const RegisterSchema = z.object({
+  name: z.string().min(1, { message: "Name is required" }),
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters" }),
+  termsCondition: z.boolean().refine((value) => value === true, {
+    message: "You must accept the terms and conditions",
+  }),
+});
