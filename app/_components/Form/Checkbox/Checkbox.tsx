aad25c50fc90e8b47e70bf75c758511cdf4ac4a4
mnawara <marcinnawara@int.pl>
2024-10-22 23:05:29 +0200

    Refactor code:  Change properties  and refactor structure component

--- app/_components/Form/Checkbox/Checkbox.tsx
+++ app/_components/Form/Checkbox/Checkbox.tsx
@@ -2,39 +2,48 @@
 
 import { InputBase } from "@/app/types";
 import { CheckIcon } from "lucide-react";
-import { useState } from "react";
+import { forwardRef } from "react";
+import InputError from "../InputError/InputError";
 
-const Checkbox = ({ label, name }: InputBase) => {
-  const [isChecked, setIsChecked] = useState(false);
+type CheckboxProps = {
+  checked: boolean;
+  onChange: () => void;
+} & InputBase;
 
-  const handleCheckbox = () => {
-    setIsChecked((prev) => !prev);
-  };
-  return (
-    <div
-      className="cursor-pointer inline-flex max-w-max items-center gap-4"
-      onClick={handleCheckbox}
-    >
-      <input
-        type="checkbox"
-        className="hidden"
-        name={name}
-        id={name}
-        defaultChecked={isChecked}
-      />
-      <div
-        className={`size-3 p-2 border-[1px] border-gray-light hover:border-bg-primary rounded-sm relative top-0 left-0 transition-all duration-300 hover:scale-110 ${
-          isChecked ? "bg-primary" : ""
-        }`}
-      >
-        <CheckIcon
-          strokeWidth={3}
-          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-3 text-white"
-        />
+const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
+  ({ label, name, checked, errors, onChange, ...rest }, ref) => {
+    return (
+      <div className="cursor-pointer inline-flex items-center gap-4 relative mb-2">
+        <label className="cursor-pointer inline-flex items-center gap-4">
+          <input
+            type="checkbox"
+            className="hidden peer"
+            id={name}
+            name={name}
+            ref={ref}
+            checked={checked}
+            onChange={onChange}
+            {...rest}
+          />
+          <div
+            className={`size-3 p-2 border-[1px] border-gray-light hover:border-bg-primary rounded-sm relative top-0 left-0 transition-all duration-300 hover:scale-110 peer-checked:bg-primary
+            }`}
+          >
+            <CheckIcon
+              strokeWidth={3}
+              className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-3 text-white"
+            />
+          </div>
+          <span className="text-sm cursor-pointer">{label}</span>
+        </label>
+        <div className="absolute -bottom-5 w-full mt-[1px]">
+          <InputError errors={errors ? errors : {}} name={name} />
+        </div>
       </div>
-      <span className="text-sm cursor-pointer">{label}</span>
-    </div>
-  );
-};
+    );
+  }
+);
+
+Checkbox.displayName = "Checkbox";
 
 export default Checkbox;
