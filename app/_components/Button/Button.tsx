d17bf9e7a989f793541e3b6716a84e145bfd462f
mnawara <marcinnawara@int.pl>
2024-11-10 13:03:31 +0100

    Refactor code: Add mew types for button

--- app/_components/Button/Button.tsx
+++ app/_components/Button/Button.tsx
@@ -6,6 +6,8 @@ import Image from "next/image";
 type ButtonProps = VariantProps<typeof button> & {
   children?: ReactNode;
   icon?: string;
+  onClick?: () => void;
+  type?: "submit" | "reset" | "button";
 };
 
 const Button = ({
@@ -17,10 +19,16 @@ const Button = ({
   fontSize,
   soft,
   icon,
+  type,
+  onClick,
+  ...rest
 }: ButtonProps) => {
   return (
     <button
       className={button({ color, rounded, outline, size, soft, fontSize })}
+      type={type}
+      onClick={onClick}
+      {...rest}
     >
       {icon && (
         <Image src={icon} height={17} className="!fill-blue" alt={icon} />
